fd7533c929fc7cb701839513b53d10340af3a7d2
Arianrhod <hiromi.kaede@gmail.com>
2024-06-16 22:50:20 +0800

    <ml::hooker> MemoryProtector

--- src/hooker/err.rs
+++ src/hooker/err.rs
@@ -5,11 +5,20 @@ pub enum HookError {
     #[error("invalid parameter")]
     InvalidParameter,
 
-    #[error("change memory protect failed: 0x{0:08X}")]
-    VirtualProtect(u32),
+    #[error("change memory protection failed: va: 0x{0:08X}, size: 0x{1:04X}, err: 0x{2:08X}")]
+    VirtualProtect(usize, usize, u32),
 
     #[error("invalid patch size: 0x{0:08X}")]
     InvalidPatchSize(usize),
+
+    #[error("buffer too small: got 0x{0:08X}, expected 0x{1:08X}")]
+    BufferTooSmall(usize, usize),
+
+    #[error("io error")]
+    Io(#[from] std::io::Error),
+
+    #[error("try from error")]
+    TryFrom(#[from] std::array::TryFromSliceError),
 }
 
 pub type Result<T> = std::result::Result<T, HookError>;
--- src/hooker/mod.rs
+++ src/hooker/mod.rs
@@ -4,3 +4,59 @@ pub mod err;
 pub mod x86;
 
 pub mod ldasm;
+
+#[cfg(target_os = "windows")]
+use windows_sys::Win32::{
+    Foundation::{GetLastError, FALSE},
+    System::Memory::{VirtualProtect, PAGE_EXECUTE_READWRITE},
+};
+
+pub struct MemoryProtector {
+    va            : usize,
+    size          : usize,
+    old_protect   : u32,
+}
+
+#[cfg(target_os = "windows")]
+impl MemoryProtector {
+    pub fn new(va: usize, size: usize) -> err::Result<Self> {
+        let mut old_protect: u32 = 0;
+
+        let success = unsafe {
+            VirtualProtect(
+                va as *const std::os::raw::c_void,
+                size,
+                PAGE_EXECUTE_READWRITE,
+                &mut old_protect,
+            )
+        };
+
+        if success == FALSE {
+            return Err(err::HookError::VirtualProtect(va, size, unsafe { GetLastError() }));
+        }
+
+        Ok(Self{
+            va,
+            size,
+            old_protect,
+        })
+    }
+}
+
+#[cfg(target_os = "windows")]
+impl Drop for MemoryProtector {
+    fn drop(&mut self) {
+        if self.old_protect == PAGE_EXECUTE_READWRITE {
+            return;
+        }
+
+        unsafe {
+            VirtualProtect(
+                self.va as *const std::os::raw::c_void,
+                self.size,
+                self.old_protect,
+                &mut self.old_protect,
+            );
+        }
+    }
+}
